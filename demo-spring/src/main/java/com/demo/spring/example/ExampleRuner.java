0161369051dbfc7f3062c0aba74b9093e77b992e
yangyang115 <yangyang36@jd.com>
2018-07-12 13:43:07 +0800

    commit

--- demo-spring/src/main/java/com/demo/spring/example/ExampleRuner.java
+++ demo-spring/src/main/java/com/demo/spring/example/ExampleRuner.java
@@ -5,20 +5,38 @@ import com.demo.spring.example.article.WebEchoArticle;
 import com.demo.spring.example.config.ConfigSource;
 import com.demo.spring.example.context.ExampleSpringConfiguration;
 import com.demo.spring.example.context.InitUccAnnotationBean;
+import com.jd.security.configsec.JDSecurityPropertyInterface;
+import com.jd.security.configsec.codec.binary.Base64;
+import com.jd.security.configsec.spring.config.JDSecurityLoader;
+import com.jd.security.org.xeustechnologies.jcl.JarClassLoader;
+import com.jd.security.org.xeustechnologies.jcl.JclObjectFactory;
 import io.vertx.core.Vertx;
+import javassist.ClassPool;
+import javassist.CtClass;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import sun.security.provider.certpath.Vertex;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by yangyang115 on 18-7-4.
  */
 public class ExampleRuner {
 
+
     public static void main(String[] srcs){
         ApplicationContext context = new AnnotationConfigApplicationContext(ExampleSpringConfiguration.class);
-        //InitUccAnnotationBean initUccAnnotationBean = (InitUccAnnotationBean)context.getBean("initUccAnnotationBean");
-        //System.out.println(JSON.toJSON(initUccAnnotationBean.getXxAdfConfigSource()));
         System.out.println(JSON.toJSON(context.getBean("configSource")));
+
     }
 }
